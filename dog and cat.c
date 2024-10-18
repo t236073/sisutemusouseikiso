b0686c0301afc42bfcf02851ca2bd4cb22841b30
t236073 <t236073@st.yamagata-u.ac.jp>
2024-10-18 19:30:30 +0900

    Add files via upload

---
+++ dog and cat.c
@@ -0,0 +1,20 @@
+int main()
+{
+    int k;
+    printf("How many dogs?");
+    scanf("%d", &k);
+    
+    printf("The number of dogs is %d",k);
+   
+    int n;
+    printf("How many cats?");
+    scanf("%d", &n);
+    
+    printf("The number of cats is %d",n);
+    
+    
+    printf("all number is %d",k+n);
+    
+    
+    return 0;
+}
